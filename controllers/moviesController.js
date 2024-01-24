f636d04cc4ce77cd47ea8d0ba8a68bc4fbf015d4
Nicole Beechler <nmbeechler@gmail.com>
2024-01-23 19:57:15 -0500

    Added movie controller code without add performer

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -13,3 +13,35 @@ router.get('/:id', movieCtrl.show)
 router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer)
 */
 
+// INDUCES
+
+exports.create = async function create(req, res) {
+    try {
+        const createdMovie = await Movie.create(req.body)
+        res.status(200).json(createdMovie)
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+}
+
+exports.index = async function(req, res) {
+    try {
+        const foundMovies = await Movie.find({})
+        res.status(200).json(foundMovies)
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+}
+
+exports.show = async function show(req, res) {
+    try {
+        const foundMovie = await Movie.findOne({_id: req.params.id})
+        res.status(200).json(foundMovie)
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+}
+
+exports.addPerformer = async function addPerformer(req, res) {
+
+}
