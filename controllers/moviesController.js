185eb947ea0b27ff19edd98eb105d7ee795c67de
Nicole Beechler <nmbeechler@gmail.com>
2024-01-23 19:41:59 -0500

    Added Movie Router code

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,2 +1,15 @@
 const Movie = require('../models/movie')
 
+
+
+/*
+// POST /movies: Accepst Movie Data and Creates A Movie
+router.post('/', movieCtrl.create)
+// GET /movies: Returns a list of all movies.
+router.get('/', movieCtrl.index)
+// GET /movies/:id: Gets An Individual Movie
+router.get('/:id', movieCtrl.show)
+// POST /movies/:movieId/performers/:performerId
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer)
+*/
+
